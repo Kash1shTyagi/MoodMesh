c8177835b32fd16a3522c478d124427c0dc59725
Kash1shTyagi <tyagikashish1001@gmail.com>
2025-06-28 21:38:49 +0530

    chore: add sys.path configuration in conftest.py and create __init__.py in utils directory

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 import os
+import sys
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 # Set environment variables for testing
 @pytest.fixture(scope="session", autouse=True)
 def setup_env():
